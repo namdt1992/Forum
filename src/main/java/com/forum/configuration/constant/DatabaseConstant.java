776fdc57005d381a9430c2155382a09b4b265382
namdt1992 <tuannamduong@gmail.com>
2017-07-10 20:00:32 +0700

    Fist commit

---
+++ src/main/java/com/forum/configuration/constant/DatabaseConstant.java
@@ -0,0 +1,5 @@
+package com.forum.configuration.constant;
+
+public class DatabaseConstant {
+public static final String schema = "nam";
+}
---
+++ src/main/java/com/forum/util/view/Public.java
@@ -0,0 +1,5 @@
+package com.forum.util.view;
+
+public class Public {
+
+}
