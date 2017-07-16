3808d934401145f52fb5dc86db07e55f55f12845
Giang-Nguyen <nhgiang94@gmail.com>
2017-07-16 22:27:25 +0800

    Fix table name error

--- src/main/java/com/forum/controller/UserAPI.java
+++ src/main/java/com/forum/controller/UserAPI.java
@@ -3,8 +3,24 @@
  */
 package com.forum.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.forum.configuration.constant.HttpConstant;
+import com.forum.domain.User;
+import com.forum.service.user.UserService;
+import com.forum.util.Jackson;
+import com.forum.util.validation.EntityStatus;
+import com.forum.util.validation.RepositoryStatus;
+import com.forum.util.validation.Status;
+import com.forum.util.view.Public;
 
 /**
  * @author NamDT8
@@ -14,10 +30,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user")
 public class UserAPI extends AbstractAPI {
-	/*
+	
 @Autowired
 private UserService userRepository;
-@GetMapping(value = "/_findOne		/all",
+
+@GetMapping(value = "/all",
 produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public ResponseEntity<String> findAll() {
 
@@ -29,11 +46,11 @@ return new ResponseEntity<String>(Jackson.java2Json(new Status(status.getCode(),
 
 return new ResponseEntity<String>
 (Jackson.java2Json
-		(new QueryStatus<User>
+		(new EntityStatus<User>
 		(HttpConstant.CODE_SUCCESS,
         "found",
         status.getObject()),
 		Public.class), HttpStatus.OK);
 }
-*/
+
 }
